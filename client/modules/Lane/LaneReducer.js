c35e36aea099db88b977fa857d3357aa01f019ad
Tobiasz18 <tobiaszpost@gmail.com>
2019-03-21 20:38:43 +0100

    reducer lane CRUD

--- client/modules/Lane/LaneReducer.js
+++ client/modules/Lane/LaneReducer.js
@@ -1,14 +1,28 @@
 // Import Actions
-import {  } from './LaneActions';
+import { CREATE_LANE, UPDATE_LANE, DELETE_LANE } from './LaneActions';
 
 // Initial State
 const initialState = {};
 
 const LaneReducer = (state = initialState, action) => {
   switch (action.type) {
+    case CREATE_LANE:
+      return [...state, action.lane];
+
+    case UPDATE_LANE:
+      return state.map((lane) => {
+        if(lane.id === action.id) {
+          return Object.assign({}, lane, action.updatedLane)
+        }
+        return lane;
+      })
+
+    case DELETE_LANE:
+      return state.fiter((lane) => lane.id !== action.id)  
     default:
       return state;
   }
-};
+};  
 
+                
 export default LaneReducer;
