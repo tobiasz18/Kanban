a2960a2f1f880550679fcd3b398ea2ae6bd6c25e
Tobiasz18 <tobiaszpost@gmail.com>
2019-03-21 15:42:14 +0100

    note actions

--- client/modules/Note/NoteActions.js
+++ client/modules/Note/NoteActions.js
@@ -1,3 +1,31 @@
-// Export Constants
+import uuid from 'uuid';
 
+// Export Constants
+export const CREATE_NOTE = 'CREATE_NOTE';
+export const UPDATE_NOTE = 'UPDATE_NOTE';
+export const DELETE_NOTE = 'DELETE_NOTE';
 // Export Actions
+
+export function createNote(note) {
+    return {
+        type: CREATE_NOTE,
+        note: {
+            id: uuid.v4(),
+            ...note
+        }
+    }
+}
+
+export function updateNote(updatedNote) {
+    return {
+        type: UPDATE_NOTE,
+        ...updatedNote
+    }
+}
+
+export function deleteNote(id) {
+    return {
+        type: DELETE_NOTE,
+        id
+    }
+}
