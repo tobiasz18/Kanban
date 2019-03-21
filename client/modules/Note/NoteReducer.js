d77a17add92a382a72c5397b0e8360684cb4446c
Tobiasz18 <tobiaszpost@gmail.com>
2019-03-21 23:04:50 +0100

    note reducer

--- client/modules/Note/NoteReducer.js
+++ client/modules/Note/NoteReducer.js
@@ -1,11 +1,20 @@
 // Import Actions
-import {  } from './NoteActions';
+import { CREATE_NOTE, UPDATE_NOTE, DELETE_NOTE } from './NoteActions';
 
 // Initial State
-const initialState = {};
+const initialState = [];
 
 const NoteReducer = (state = initialState, action) => {
   switch (action.type) {
+    case CREATE_NOTE:
+      return [...state, action.note]      
+
+    case UPDATE_NOTE: 
+      return state.map((note) => note.id === action.id ? { ...note, ...action.note } : note );  
+
+    case DELETE_NOTE:
+      return state.filter(note => note.id !== action.id)
+      
     default:
       return state;
   }
