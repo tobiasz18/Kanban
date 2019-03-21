9024f6603e2a6aa2aff0b9fe41733898e855ad6a
Tobiasz18 <tobiaszpost@gmail.com>
2019-03-21 15:08:44 +0100

    lane actions

--- client/modules/Lane/LaneActions.js
+++ client/modules/Lane/LaneActions.js
@@ -1,3 +1,40 @@
+import uuid from 'uuid';
+
 // Export Constants
+export const CREATE_LANE = 'CREATE_LANE';
+export const UPDATE_LANE = 'UPDATE_LANE';
+export const DELETE_LANE = 'DELETE_LANE';
+export const CREATE_LANES = 'CREATE_LANES';
 
 // Export Actions
+export function createLane(lane) {
+    return {
+        type: CREATE_LANE,
+        lane: {
+            id: uuid.v4(),
+            notes: lane.notes || [],
+            ...lane
+        }
+    };
+};
+
+export function updateLane(updatedLane) {
+    return {
+        type: UPDATE_LANE,
+        ...updatedLane
+    };
+};
+
+export function deleteLane(id) {
+    return {
+        type: DELETE_LANE,
+        id
+    }
+}
+
+export function createLanes(lanes) {
+    return {
+        type: CREATE_LANES,
+        lanes
+    }
+}
