210d62a413b5b8c7a7ff710dede02bceda41c437
Tobiasz18 <tobiaszpost@gmail.com>
2019-03-23 13:19:12 +0100

    presentation components

--- client/modules/Lane/Lane.js
+++ client/modules/Lane/Lane.js
@@ -1,30 +1,38 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React, { Component, PropTypes } from 'react';
 
 // Import Style
 import styles from './Lane.css';
 
 class Lane extends Component {
   render() {
+    const { lane, laneNotes, ...props } = this.props;
+    const laneId = lane.id
+
     return (
+      <div {...props}>
+        <div
+          className={styles.LaneHeader}
+          onClick={() => props.updateLane({ id: laneId, editing: true })}
+        >
+          <div>
+            <button onClick={props.addNote.bind(this. laneId)}>+</button>
+          </div>
+          <h4>{lane.name}</h4>
+          <div className={styleMedia.LaneDelete}>
+            <button onClick={this.props.LaneDelete.bind(this, lane)}>x</button>
+          </div>
+        </div>
+        <NotesContainer 
+          notes={laneNotes}
+        />
+      </div>
     );
   }
 }
 
-const mapStateToProps = (state) => {
-  return {};
-};
+Lane.propsTypes = {
+  lane: PropTypes.object,
+  laneNotes: PropTypes.array
+}
 
-const mapDispatchToProps = (dispatch) => {
-  return {};
-};
-
-Lane.propTypes = {
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Lane);
+export default Lane;
